64ee8d8ef499a748817a4895b1c6e9ba918c698c
Harun Bekcan <harun.bekcan@applogist.com>
2022-08-08 14:57:57 +0300

    FirstCommit

---
+++ app/src/main/java/com/harunbekcan/sampleandroidproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.harunbekcan.sampleandroidproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
