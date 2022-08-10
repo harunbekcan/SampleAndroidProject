3c047a2a325953d49af94e615363b22b6257a6b1
Harun Bekcan <harun.bekcan@applogist.com>
2022-08-10 22:52:29 +0300

    Removed unused imports on homepage

--- app/src/main/java/com/harunbekcan/sampleandroidproject/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/harunbekcan/sampleandroidproject/ui/fragment/HomeFragment.kt
@@ -1,10 +1,6 @@
 package com.harunbekcan.sampleandroidproject.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.harunbekcan.sampleandroidproject.R
 import com.harunbekcan.sampleandroidproject.base.BaseFragment
 import com.harunbekcan.sampleandroidproject.data.BottomSheetModel
